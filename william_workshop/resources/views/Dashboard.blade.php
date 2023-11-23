f702d766caa76f1ad8f9ff76cdd0ed5be39eb48b
ATSAindonesia <dsfiteoffical@gmail.com>
2023-11-23 16:57:39 +0700

    First commit

---
+++ william_workshop/resources/views/Dashboard.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>BELAJAR</title>
+</head>
+
+<body>
+<h1>Nama : Alfonsus William Hamonangan Sinaga</h1>
+<h2>Kelas/Prodi : 1A/Teknologi Rekaya Perangkat Lunak</h2>
+</body>
+
+</html> 
